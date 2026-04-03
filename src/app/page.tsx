89688ef324f5961226054c57e652914f5eb5c105
yoychen <yui12327@gmail.com>
2026-04-03 23:09:54 +0800

    Tweak example page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,14 +20,6 @@ export default function Home() {
           priority
         />
 
-        <List
-          items={[
-            { id: 1, label: "Learn Next.js" },
-            { id: 2, label: "Build with TypeScript" },
-            { id: 3, label: "Deploy to Vercel" },
-          ]}
-        />
-
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
@@ -51,6 +43,15 @@ export default function Home() {
             center.
           </p>
         </div>
+
+        <List
+          items={[
+            { id: 1, label: "Learn Next.js" },
+            { id: 2, label: "Build with TypeScript" },
+            { id: 3, label: "Deploy to Vercel" },
+          ]}
+        />
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
